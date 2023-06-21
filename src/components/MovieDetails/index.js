942984918dd72fc5cf710df06c1678fe33a3ab87
James Yu Cao <caoyu19920414@gmail.com>
2023-06-20 22:45:05 -0400

    finished

--- src/components/MovieDetails/index.js
+++ src/components/MovieDetails/index.js
@@ -1,7 +1,18 @@
-function MovieDetails() {
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+function MovieDetails({ movies }) {
+  console.log(movies);
+  const { movieId } = useParams();
+  const movieChoice = movies.find(item => {
+    if (item.id === parseInt(movieId)) {
+      return true
+    } else {
+      return false
+    }
+  });
   return (
     <div className='comp purple'>
-      <h1>MovieDetails Component</h1>
+      <h1>{movieChoice.title}</h1>
+      <p>{movieChoice.description}</p>
     </div>
   );
 }
--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,8 +1,34 @@
-function Movies() {
+import { Route } from 'react-router-dom'
+import MovieDetails from '../MovieDetails';
+import { NavLink, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+
+function Movies(props) {
+  const movies = props.movies;
+  //console.log(movies);
   return (
     <div className='comp orange'>
       <h1>Movies Component</h1>
-    </div>
+      <nav className='comp nav'>
+        <ul>
+          {movies.map(item => {
+            const { id, title, description } = item;
+            const url = '/movies/' + id;
+            return <li key={id}>
+              <NavLink
+                activeClassName='purple'
+                activeStyle={{ fontWeight: 'bold' }}
+                to={url}>
+                {title}</NavLink>
+            </li>;
+          })}
+        </ul>
+      </nav>
+      <Switch>
+        <Route path='/movies/:movieId'>
+          <MovieDetails movies={movies} />
+        </Route>
+      </Switch>
+    </div >
   );
 }
 
